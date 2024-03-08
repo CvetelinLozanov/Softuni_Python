68a0f171c8a4371dbb0ce8a0326d38717be16e1e
CvetelinLozanov <cvelozanov@gmail.com>
2024-03-08 20:54:43 +0200

    fix solution for problem 9

--- softuni_fundamentals/text_processing_exercise/09_rage_quit.py
+++ softuni_fundamentals/text_processing_exercise/09_rage_quit.py
@@ -6,8 +6,8 @@ def process_text(text_: List[str]):
     current_index = 0
     result = ''
     #p = re.findall("[0-9]+", text_)
-    for index in range(len(text_) - 1):
-        if text_[index].isdigit() and text[index + 1].isdigit():
+    for index in range(len(text_)):
+        if index < len(text_) - 1 and text_[index].isdigit() and text[index + 1].isdigit():
             times_to_repeat = int(str(text_[index]) + str(text_[index + 1]))
             text_to_add = ''.join(text_[current_index:index] * times_to_repeat)
             result += text_to_add
@@ -21,5 +21,13 @@ def process_text(text_: List[str]):
     return result
 
 
+def get_unique_symbols_count(rage_message: str):
+    return len(set(list(rage_message)))
+
+
+
 text = [ch.upper() for ch in list(input())]
-print(process_text(text))
+rage_message = process_text(text)
+unique_symbols_count = get_unique_symbols_count(rage_message)
+print(f'Unique symbols used: {unique_symbols_count}')
+print(rage_message)
