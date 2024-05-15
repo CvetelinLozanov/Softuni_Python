b4571e7aefb6aae1e3872c3c5df36678b9ffc3d5
CvetelinLozanov <cvelozanov@gmail.com>
2024-05-15 20:50:07 +0300

    Update solution for fast food solution.

--- python_advanced/list_and_stacks_and_queues_exercise/fast_food.py
+++ python_advanced/list_and_stacks_and_queues_exercise/fast_food.py
@@ -1,39 +1,28 @@
 from collections import deque
-from sys import  maxsize
-
-
-def check_max_value_in_stack(max_value, value):
-    return max_value < value
 
 
 food_quantity = int(input())
-quantity_of_food_per_client = deque(input().split())
-biggest_order = -maxsize
+orders = deque([int(num) for num in input().split()])
 
-for index in range(len(quantity_of_food_per_client)):
-    current_order = int(quantity_of_food_per_client[index])
 
-    if check_max_value_in_stack(biggest_order, current_order):
-        biggest_order = current_order
+for index in range(len(orders)):
+    current_order = int(orders[index])
 
-print(biggest_order)
 
-while len(quantity_of_food_per_client) != 0:
-    current_order = int(quantity_of_food_per_client[0])
+print(max(orders))
+
+while orders:
+    current_order = int(orders[0])
 
     if current_order <= food_quantity:
-        quantity_of_food_per_client.popleft()
+        orders.popleft()
         food_quantity -= current_order
 
     else:
         break
 
-if len(quantity_of_food_per_client) == 0:
+if len(orders) == 0:
     print('Orders complete')
 
 else:
-    print('Orders left: ', end='')
-    while quantity_of_food_per_client:
-        print(quantity_of_food_per_client.popleft(), end='')
-        if quantity_of_food_per_client:
-            print(', ', end='')
+    print(f"Orders left: {' '.join(map(str,orders))}")
