91e08ef6e7db0700cd8d4b23f3b12520310486b2
CvetelinLozanov <cvelozanov@gmail.com>
2024-07-06 22:40:55 +0300

    Adding solutions for exam 18_04_2022

--- Python_OOP/exams/exam_18_04_2022/project/movie_app.py
+++ Python_OOP/exams/exam_18_04_2022/project/movie_app.py
@@ -24,35 +24,87 @@ class MovieApp:
         if not user:
             raise Exception("This user does not exist!")
 
-        if movie.owner != user:
-            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
-
         if movie in user.movies_owned:
             raise Exception("Movie already added to the collection!")
+
+        if movie.owner.username != user.username:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
 
         user.movies_owned.append(movie)
         self.movies_collection.append(movie)
         return f"{username} successfully added {movie.title} movie."
 
     def edit_movie(self, username: str, movie: Movie, **kwargs):
-        pass
+        user = self._get_user(username)
+        if movie.owner != user:
+            raise Exception(f"{username} is not the owner of the movie {movie.title}!")
+
+        if movie not in self.movies_collection:
+            raise Exception(f"The movie {movie.title} is not uploaded!")
+
+        for key, value in kwargs.items():
+            setattr(movie, key, value)
+
+        return f"{username} successfully edited {movie.title} movie."
 
     def delete_movie(self, username: str, movie: Movie):
-        pass
+        if movie not in self.movies_collection:
+            raise Exception(f"The movie {movie.title} is not uploaded!")
+
+        user = self._get_user(username)
+
+        if user.username != movie.owner.username:
+            raise Exception(f"{user.username} is not the owner of the movie {movie.title}!")
+
+        self.movies_collection.remove(movie)
+        user.movies_owned.remove(movie)
+        return f"{username} successfully deleted {movie.title} movie."
 
     def like_movie(self, username: str, movie: Movie):
-        pass
+        user = self._get_user(username)
+
+        if user.username == movie.owner.username:
+            raise Exception(f"{username} is the owner of the movie {movie.title}!")
+
+        if movie in user.movies_liked:
+            raise Exception(f"{username} already liked the movie {movie.title}!")
+
+        movie.likes += 1
+        user.movies_liked.append(movie)
+        return f"{username} liked {movie.title} movie."
 
     def dislike_movie(self, username: str, movie: Movie):
-        pass
+        user = self._get_user(username)
+        if movie not in user.movies_liked:
+            raise Exception(f"{username} has not liked the movie {movie.title}!")
+
+        movie.likes -= 1
+        user.movies_liked.remove(movie)
+        return f"{username} disliked {movie.title} movie."
 
     def display_movies(self):
-        pass
+        if not self.movies_collection:
+            return "No movies found."
+
+        sorted_movies = sorted(self.movies_collection, key=lambda m: (-m.year, m.title))
+        result = []
+        for movie in sorted_movies:
+            result.append(movie.details())
+
+        return "\n".join(result)
 
     def __str__(self):
-        pass
+        users = f"All users: {', '.join([u.username for u in self.users_collection])}" if self.users_collection \
+            else "All users: No users."
+        movies = f"All movies: {', '.join([m.title for m in self.movies_collection])}" if self.movies_collection\
+            else "All movies: No movies."
+
+        return users + '\n' + movies
 
     def _get_user(self, username):
         return next((u for u in self.users_collection if u.username == username), None)
+
+    def _get_movie(self, movie: Movie):
+        return next((m for m in self.movies_collection if m.title == movie.title), None)
 
 
